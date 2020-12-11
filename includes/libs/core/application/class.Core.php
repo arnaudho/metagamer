00e0d200fbda416cbb2af4e486707fb66331bb16
Romain Soufflet <romain@soufflet.io>
2020-12-11 11:22:38 +0100

    Dirty Commit: Force HTTPS schema for URLs

--- includes/libs/core/application/class.Core.php
+++ includes/libs/core/application/class.Core.php
@@ -210,7 +210,7 @@ namespace core\application
 		static public function parseURL($pUrl = null)
 		{
 			Configuration::$server_domain = $_SERVER["SERVER_NAME"];
-			$protocol = "http".((isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443')?'s':'')."://";
+			$protocol = "https".((isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443')?'s':'')."://";
 			Configuration::$server_folder = preg_replace('/\/(index).php$/', "", $_SERVER["SCRIPT_NAME"]);
 			Configuration::$server_folder = preg_replace('/^\//', "", Configuration::$server_folder);
 			Configuration::$server_url = $protocol.Configuration::$server_domain."/";
