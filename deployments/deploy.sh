e6c7e7897dc2adee051df785a6de168850b4c85d
Romain Soufflet <romain@soufflet.io>
2020-12-23 16:54:25 +0100

    Stop creating unused yaml files

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -25,24 +25,22 @@ EOF
 
 sed "s/mtg.kub.soufflet.io/${DEPLOYMENT_NAME}.kub.soufflet.io/g" ${SCRIPT_DIR}/../includes/applications/setup.json > metagamer_setup.json
 sed "s/MYSQL_PASSWORD/${MYSQL_ROOT_PASSWORD}/g" ${SCRIPT_DIR}/../includes/applications/kube.config.json > metagamer_config.json
+
+kubectl create namespace ${GITHUB_RUN_ID}
 kubectl create configmap metagamer-config \
   --from-file=metagamer_setup.json \
   --from-file=metagamer_config.json \
-  --dry-run=client -o yaml > metagamer_config.yml
-
+  --namespace ${GITHUB_RUN_ID}
 kubectl create configmap sql-init \
   --from-file=schema.sql \
   --from-file=users.sql \
-  --dry-run=client -o yaml > configmap.yml
-
+  --namespace ${GITHUB_RUN_ID}
 kubectl create secret generic mysql-secret \
-  --from-literal=ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
-  --dry-run=client -o yaml > secret.yml
+  --from-literal=ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 
 alias kustomize="${SCRIPT_DIR}/../kustomize"
 kustomize edit set namespace ${GITHUB_RUN_ID}
 kustomize edit set image gcr.io/PROJECT_ID/IMAGE:TAG=gcr.io/$PROJECT_ID/$IMAGE:$GITHUB_SHA
-kustomize build .
 kustomize build . | kubectl apply -f -
 kubectl rollout status deployment/$DEPLOYMENT_NAME
 kubectl get services -o wide
